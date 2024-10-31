31368c3c9782e0a653ad18e37bc1d0c3e5238faa
manyids2 <arun.mukundan@gmail.com>
2024-10-31 18:45:10 +0100

    Without md5 check for local_tests.pickle

--- README.md
+++ README.md
@@ -1,4 +1,75 @@
 ![uva-logo](src/uva-logo.jpg)
 
 # Applied Machine Learning 2024
+
 Lab assignments for the course Applied Machine Learning at the University of Amsterdam.
+
+## week 1
+
+week_1/Intro.ipynb
+
+| function             | args                   |
+| -------------------- | ---------------------- |
+| w1_linear_forward    | ['x_input', 'P']       |
+| w1_cal_pseudoinverse | ['x_input', 'y_input'] |
+| w1_L2_regression     | ['x_input', 'y_input'] |
+
+## week 2
+
+week_2/ML.ipynb
+
+| function                    | args                                       |
+| --------------------------- | ------------------------------------------ |
+| w2_linear_forward           | ['x_input', 'W', 'b']                      |
+| w2_linear_grad_W            | ['x_input', 'grad_output', 'W', 'b']       |
+| w2_linear_grad_b            | ['x_input', 'grad_output', 'W', 'b']       |
+| w2_sigmoid_forward          | ['x_input']                                |
+| w2_sigmoid_grad_input       | ['x_input', 'grad_output']                 |
+| w2_nll_forward              | ['target_pred', 'target_true']             |
+| w2_nll_grad_input           | ['target_pred', 'target_true']             |
+| w2_dist_to_training_samples | ['x_input', 'training_set']                |
+| w2_nearest_neighbors        | ['distances', 'training_labels']           |
+| w2_tree_weighted_entropy    | ['Y_left', 'Y_right', 'classes']           |
+| w2_tree_split_data_left     | ['X', 'Y', 'feature_index', 'split_value'] |
+| w2_tree_split_data_right    | ['X', 'Y', 'feature_index', 'split_value'] |
+| w2_tree_to_terminal         | ['Y']                                      |
+
+## week 3
+
+week_3/Neural_Nets.ipynb
+
+| function           | args                        |
+| ------------------ | --------------------------- |
+| w3_dense_forward   | ['x_input', 'W', 'b']       |
+| w3_relu_forward    | ['x_input']                 |
+| w3_l2_regularizer  | ['weight_decay', 'weights'] |
+| w3_conv_matrix     | ['matrix', 'kernel']        |
+| w3_box_blur        | ['image', 'box_size']       |
+| w3_maxpool_forward | ['x_input']                 |
+| w3_flatten_forward | ['x_input']                 |
+
+## list
+
+w1_linear_forward
+w1_cal_pseudoinverse
+w1_L2_regression
+w2_linear_forward
+w2_linear_grad_W
+w2_linear_grad_b
+w2_sigmoid_forward
+w2_sigmoid_grad_input
+w2_nll_forward
+w2_nll_grad_input
+w2_dist_to_training_samples
+w2_nearest_neighbors
+w2_tree_weighted_entropy
+w2_tree_split_data_left
+w2_tree_split_data_right
+w2_tree_to_terminal
+w3_dense_forward
+w3_relu_forward
+w3_l2_regularizer
+w3_conv_matrix
+w3_box_blur
+w3_maxpool_forward
+w3_flatten_forward
--- week_2/automark.py
+++ week_2/automark.py
@@ -1,62 +1,81 @@
-from __future__ import print_function
-import os
-import sys
-import json
-import requests
-import shutil
-import numpy as np
 import hashlib
+import json
+import os
+import pickle
+import shutil
+from pathlib import Path
 
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
-
-try:
-    FileNotFoundError
-except NameError:
-    FileNotFoundError = OSError
+import numpy as np
+import requests
 
 
 class ServerError(BaseException):
     pass
 
 
-HOST = "161.35.93.12"
-
+# class Config:
+#     host = "http://127.0.0.1:1234"
+#     cwd = Path("./")
+#     test_folder = Path("data/assignments")
+#     test_path = test_folder / "local_tests.pickle"
 
 class Config:
-    host = f"http://{HOST}:1234/"
-    cwd = os.path.dirname(os.path.realpath(__file__))
-    test_folder = os.path.join(cwd, "local_tests")
-    test_path = os.path.join(test_folder, "tests.pickle")
+    host = "http://161.35.93.12:1234"
+    cwd = Path("./")
+    test_folder = Path("local_tests")
+    test_path = test_folder / "local_tests.pickle"
+
+
+def get_progress(username):
+    endpoint = f"{Config.host}/get_progress/{username}"
+    response = requests.get(endpoint)
+    data = response.json()
+
+    if "error" in data:
+        raise ServerError(data["error"])
+
+    completed = 0
+    for k, v in data["progress"].items():
+        if v == "completed":
+            completed += 1
+    completed_percentage = round(completed / len(data["progress"]) * 100)
+
+    print(
+        "| Current Assignment Grade {:2}%              |".format(completed_percentage)
+    )
+    for k, v in sorted(data["progress"].items()):
+        print("| {:25}| {:15}|".format(k, v))
 
 
 # Local tests
 def _remove_local_tests():
     try:
-        os.remove(Config.test_path)
+        Config.test_path.unlink()
         print(
             "Current version of local tests is deprecated. Outdated tests are removed."
         )
-        sys.stdout.flush()
     except FileNotFoundError:
         pass
 
 
-def _load_local_tests(username):
+def _download_local_tests(username):
+    """Download local_tests.pickle to client.
+
+    - Requests from endpoint: /load_tests/{username}
+      - endpoint uses LOCAL_TESTS on server
+
+    """
     if not os.path.exists(Config.test_folder):
         os.makedirs(Config.test_folder)
 
     try:
-        endpoint = Config.host + "load_tests/{}".format(username)
+        endpoint = f"{Config.host}/load_tests/{username}"
         stream = requests.get(endpoint, stream=True)
         if stream.status_code == 200:
             with open(Config.test_path, "wb") as f:
                 stream.raw.decode_content = True
                 shutil.copyfileobj(stream.raw, f)
             print("Local tests are downloaded.")
-            sys.stdout.flush()
         else:
             error = stream.json()["error"]
             raise ServerError(error)
@@ -67,13 +86,20 @@ def _load_local_tests(username):
 
 
 def _local_tests_are_valid():
+    """Confirm that local_tests.pickle is the same on server and client.
+
+    - Computes md5 hash for local_tests.pickle on client
+    - Sends to endpoint: /check_sum/{local_md5}
+      - endpoint checks LOCAL_TESTS on server
+
+    """
     try:
         hash_md5 = hashlib.md5()
         with open(Config.test_path, "rb") as f:
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
         local_md5 = hash_md5.hexdigest()
-        endpoint = Config.host + "check_sum/{}".format(local_md5)
+        endpoint = f"{Config.host}/check_sum/{local_md5}"
         response = requests.get(endpoint).json()
         return response["success"]
     except:
@@ -81,6 +107,15 @@ def _local_tests_are_valid():
 
 
 def _passed_local_tests(function, arg_keys):
+    """Execute functions and check results of local tests.
+
+    - No communication with endpoints.
+    - Assumes data has keys: "inputs", "outputs"
+      - each is an array of 100 examples
+      - inputs further are dicts with arg_keys as keys
+      - check shape, within tolerance
+
+    """
     with open(Config.test_path, "rb") as f:
         try:
             test_data = pickle.load(f, encoding="latin1")
@@ -103,7 +138,31 @@ def _passed_local_tests(function, arg_keys):
 
 # Remote tests
 def _passed_remote_test(username, function, arg_keys):
-    endpoint = Config.host + "get_test_input/{}/{}".format(username, function.__name__)
+    """Check results with remote tests.
+
+    - Contacts: /get_test_input/{username}/{function.__name__}
+        gets  : {
+            "username": username,
+            "ipd": int(Global.data_dict[assignment]["ipd"][random_ipd]),  # REMOTE_TESTS
+            "input": Global.data_dict[assignment]["inputs"][random_ipd],  # REMOTE_TESTS
+        }
+
+        assumes:
+            - input is a dict with keys as args,
+            - values as dicts with keys: "data", "type"
+            - if type is ndarray, ensure with `np.array(...)`
+
+            data["input"]
+                - X_in : {"data": ..., "type": ...}
+                - Y_in : {"data": ..., "type": ...}
+
+    - Runs tests with received inputs
+    - Contacts: /check_answer/{username}/{function.__name__}/{data["ipd"]}/{json.dumps(test_result)}
+      - sends test result with `np.array(test_result).tolist()`
+        - which gets passed to json.dumps(...)
+
+    """
+    endpoint = f"{Config.host}/get_test_input/{username}/{function.__name__}"
     response = requests.get(endpoint)
     data = response.json()
 
@@ -120,10 +179,7 @@ def _passed_remote_test(username, function, arg_keys):
 
     test_result = function(*args)
     test_result = np.array(test_result).tolist()
-    endpoint = Config.host
-    endpoint += "check_answer/{}/{}/{}/{}".format(
-        username, function.__name__, data["ipd"], json.dumps(test_result)
-    )
+    endpoint = f'{Config.host}/check_answer/{username}/{function.__name__}/{data["ipd"]}/{json.dumps(test_result)}'
 
     response = requests.get(endpoint)
     if not response.status_code == 200:
@@ -133,38 +189,16 @@ def _passed_remote_test(username, function, arg_keys):
     return answer_response["success"]
 
 
-def get_progress(username):
-    endpoint = Config.host + "get_progress/{}".format(username)
-    response = requests.get(endpoint)
-    data = response.json()
-
-    if "error" in data:
-        raise ServerError(data["error"])
-
-    completed = 0
-    for k, v in data["progress"].items():
-        if v == "completed":
-            completed += 1
-    completed_percentage = round(completed / len(data["progress"]) * 100)
-
-    print(
-        "| Current Assignment Grade {:2}%              |".format(completed_percentage)
-    )
-    for k, v in sorted(data["progress"].items()):
-        print("| {:25}| {:15}|".format(k, v))
-
-
-def test_student_function(username, function, arg_keys):
+def test_student_function_with_download(username, function, arg_keys):
     if not _local_tests_are_valid():
         _remove_local_tests()
         print("Downloading local tests...")
-        _load_local_tests(username)
+        _download_local_tests(username)
 
     print("Running local tests...")
     if _passed_local_tests(function, arg_keys):
         print("{} successfully passed local tests".format(function.__name__))
         print("Running remote test...")
-        sys.stdout.flush()
 
         if _passed_remote_test(username, function, arg_keys):
             print("Test was successful. Congratulations!")
@@ -172,3 +206,24 @@ def test_student_function(username, function, arg_keys):
             print("Test failed. Please review your code.")
     else:
         print("{} failed some local tests".format(function.__name__))
+
+
+def test_student_function(username, function, arg_keys):
+    print("Running local tests...")
+    if _passed_local_tests(function, arg_keys):
+        print("{} successfully passed local tests".format(function.__name__))
+        print("Running remote test...")
+
+        if _passed_remote_test(username, function, arg_keys):
+            print("Test was successful. Congratulations!")
+        else:
+            print("Test failed. Please review your code.")
+    else:
+        print("{} failed some local tests".format(function.__name__))
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("USAGE: python automark.py USERNAME")
+    username = sys.argv[1]
+    get_progress(username)
